6bd90ab7a376dc197f554787bf046fd5c51cac18
markrepedersen <markrepedersen@gmail.com>
2020-11-07 17:56:30 -0800

    Add test example

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ use nom::{
         streaming::take,
         streaming::{tag, take_while},
     },
-    character::is_alphanumeric,
+    character::{is_alphanumeric, streaming::crlf},
     multi::many_till,
     sequence::delimited,
 };
 use parse::{Input, ParseResult};
-use std::str::from_utf8;
-use std::str::FromStr;
+use std::{
+    io::Read,
+    net::TcpStream,
+    str::{from_utf8, FromStr},
+};
 use strum_macros::EnumString;
 
 #[derive(Clap)]
@@ -20,16 +23,39 @@ use strum_macros::EnumString;
 pub struct CLI {
     #[clap(short, long, about = "The URL of the request.")]
     pub url: String,
-}
 
-/**
-* Consumes two bytes, which equals a CRLF.
-**/
-pub fn take_crlf(i: Input) -> ParseResult<Input> {
-    take(2usize)(i)
+    #[clap(
+        short,
+        long,
+        default_value = "80",
+        about = "The port to use. Uses :80 by default."
+    )]
+    pub port: String,
 }
 
 //-------------- REQUEST ------------------
+
+#[derive(Debug)]
+pub struct Request<'a> {
+    status: RequestStatus<'a>,
+    headers: Headers<'a>,
+    body: Body,
+}
+
+impl<'a> Request<'a> {
+    pub fn parse(i: Input<'a>) -> ParseResult<Self> {
+        let (i, status) = RequestStatus::parse(i)?;
+        let (i, headers) = Headers::parse(i)?;
+        let (i, body) = Body::parse(i)?;
+        let res = Self {
+            status,
+            headers,
+            body,
+        };
+
+        Ok((i, res))
+    }
+}
 
 #[derive(Debug, PartialEq, EnumString)]
 pub enum Method {
@@ -45,12 +71,13 @@ pub enum Method {
 
 impl Method {
     pub fn parse(i: Input) -> ParseResult<Self> {
-        let (i, method) = delimited(take_crlf, take_while(is_alphanumeric), tag(b" "))(i)?;
+        let (i, method) = delimited(crlf, take_while(is_alphanumeric), tag(b" "))(i)?;
 
         Ok((i, Method::from_str(from_utf8(method).unwrap()).unwrap()))
     }
 }
 
+#[derive(Debug)]
 pub struct RequestStatus<'a> {
     pub method: Method,
     pub path: &'a str,
@@ -77,6 +104,7 @@ impl<'a> RequestStatus<'a> {
     }
 }
 
+#[derive(Debug)]
 pub struct Header<'a> {
     key: &'a str,
     value: &'a str,
@@ -84,7 +112,7 @@ pub struct Header<'a> {
 
 impl<'a> Header<'a> {
     pub fn parse(i: Input<'a>) -> ParseResult<Self> {
-        let (i, key) = delimited(take_crlf, take_while(is_alphanumeric), tag(b":"))(i)?;
+        let (i, key) = delimited(crlf, take_while(is_alphanumeric), tag(b":"))(i)?;
         let (i, value) = take_while(is_alphanumeric)(i)?;
         let res = Self {
             key: from_utf8(key).unwrap(),
@@ -95,6 +123,7 @@ impl<'a> Header<'a> {
     }
 }
 
+#[derive(Debug)]
 pub struct Headers<'a> {
     headers: Vec<Header<'a>>,
 }
@@ -107,55 +136,75 @@ impl<'a> Headers<'a> {
     }
 }
 
-pub enum Body<'a> {
-    Single(SinglePartBody<'a>),
-    Multi(MultiPartBody<'a>),
+#[derive(Debug)]
+pub enum Body {
+    Single(SinglePartBody),
+    Multi(MultiPartBody),
 }
 
-pub struct SinglePartBody<'a> {
-    data: &'a str,
+impl Body {
+    pub fn parse(i: Input) -> ParseResult<Self> {
+        let (i, body) = SinglePartBody::parse(i)?;
+        Ok((i, Body::Single(body)))
+    }
 }
 
-pub struct MultiPartBody<'a> {
-    data: Vec<&'a str>,
+#[derive(Debug)]
+pub struct SinglePartBody {
+    data: Vec<u8>,
 }
 
-pub struct Request<'a> {
-    status: RequestStatus<'a>,
-    body: Body<'a>,
+impl SinglePartBody {
+    pub fn parse(i: Input) -> ParseResult<Self> {
+        let data = vec![];
+
+        Ok((i, Self { data }))
+    }
+}
+
+#[derive(Debug)]
+pub struct MultiPartBody {
+    data: Vec<u8>,
 }
 
 // -------------------- RESPONSE---------------------
 
+#[derive(Debug)]
 pub enum StatusCode {
     Success = 200,
     NotFound = 404,
 }
 
+#[derive(Debug)]
 pub struct Status<'a> {
     pub protocol_version: &'a str,
     pub status_code: StatusCode,
     pub description: &'a str,
 }
 
+#[derive(Debug)]
 pub struct SingleResourceResponseBody<'a> {
     pub file: &'a str,
 }
 
+#[derive(Debug)]
 pub struct ChunkedResponseBody<'a> {
     pub file: &'a str,
 }
 
+#[derive(Debug)]
 pub struct MultiResourceResponseBody<'a> {
     pub files: Vec<SingleResourceResponseBody<'a>>,
 }
 
+#[derive(Debug)]
 pub enum ResponseBody<'a> {
     Single(SingleResourceResponseBody<'a>),
     ChunkedSingle(ChunkedResponseBody<'a>),
     Multi(MultiResourceResponseBody<'a>),
 }
 
+#[derive(Debug)]
 pub struct Response<'a> {
     pub status: Status<'a>,
     pub headers: Headers<'a>,
@@ -164,6 +213,14 @@ pub struct Response<'a> {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli: CLI = CLI::parse();
+    let mut stream = TcpStream::connect(cli.url + &cli.port)?;
+    let mut buf: Vec<u8> = vec![];
+
+    stream.read_to_end(&mut buf)?;
+
+    if let Ok(req) = Request::parse(&buf) {
+        dbg!(req);
+    }
 
     Ok(())
 }
